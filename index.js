0f3545b191bd05ed7732824bc33dff860f8edbc4
Jay-7374 <jayaprakash99956@gmail.com>
2025-06-04 14:56:14 +0530

    feat: update MongoDB driver to version 6.17.0 and add new models and routes
    - Updated MongoDB driver to version 6.17.0 in package.json and package-lock.json.
    - Added new Mongoose models for Order, Product, and User.
    - Implemented routes for creating and retrieving orders, products, and user registration/login.
    - Enhanced update operations to support sorting and atomic operators validation.
    - Introduced new error handling for MongoClient closure and connection management.

--- index.js
+++ index.js
@@ -1,56 +1,36 @@
-import express from 'express'
-import cors from 'cors';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+
+dotenv.config();
 
 const app = express();
-
-app.listen(8080, () => {
-  mongoose.connect("mongodb://localhost:27017/gcet");
-  console.log('Server Started');
-});
-
-const userSchema = new mongoose.Schema({
-  name :{type: String},
-  email: {type: String},
-  pass: {type: String}
-});
-
-const user = mongoose.model("users", userSchema);
-
-const productSchema = new mongoose.Schema({
-  name: { type: String },
-  price: { type: Number }
-});
-
-const Product = mongoose.model("products", productSchema);
-
-
 app.use(cors());
 app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI
+
+app.use("/users", userRouter);
+app.use("/products", productRouter);
+app.use("/orders",orderRouter)
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 app.get("/", (req, res) => {
  return res.send("Good Morning");
-});
-
-app.post("/register", async (req,res) => {
-  const {name,email,pass} = req.body;
-  const result = await user.insertOne({name: name, email: email, pass: pass});
-  return res.json(result);
-});
-
-
-app.post("/login", async (req,res) => {
-  const {email, pass} = req.body;
-  const result = await user.findOne({email: email, pass: pass});
-  if(result) {
-    return res.json({status: "ok", data: result});
-  } else {
-    return res.json({status: "error", data: "Invalid Credentials"});
-  }
-});
-
-app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
 });
 
 app.get("/greet", (req,res) => {res.send("Greetings!")});
@@ -58,4 +38,3 @@ app.get("/greet", (req,res) => {res.send("Greetings!")});
 app.get("/name", (req,res) => {res.send("Jaya Prakash")});
 
 app.get("/weather", (req,res) => {res.send("31°C")});
-
