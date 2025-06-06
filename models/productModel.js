ad9ab85483c4d761f487fa6dad967d64ad62ecfd
Jay-7374 <jayaprakash99956@gmail.com>
2025-06-06 10:27:31 +0530

    `Added image and disc fields to productSchema`

--- models/productModel.js
+++ models/productModel.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 const productSchema = mongoose.Schema({
+  image: { type: String },
   name: { type: String },
+  disc : { type: String },
   price: { type: Number },
 });
 // const product = mongoose.model("Product", productSchema);
